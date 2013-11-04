c7f052d72dd1ca0cd49e462df5ab31e28a43c9e4
U-JAVAMACHINE\Administrator <Administrator@JAVAMACHINE.(none)>
2013-11-04 00:42:25 -0500

    fixed jpa mapping error

---
+++ src/java/edu/rit/notification/service/Response.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rit.notification.service;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Administrator
+ */
+@XmlRootElement
+public abstract class Request {
+    
+}
---
+++ src/java/edu/rit/notification/service/SubscriptionRequest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rit.notification.service;
+
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class EnrollmentRequest extends Request implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1569490395248082125L;
+
+	
+	private String studentId;
+	private String classNumber;
+	
+	
+	public EnrollmentRequest(){
+		
+	}
+	
+	public EnrollmentRequest(String studentId, String classNumber) {
+		super();
+		this.studentId = studentId;
+		this.classNumber = classNumber;
+		System.out.println("Subscription Request created: StudentId: " + studentId + " classNumber: " + classNumber);
+	}
+	
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getClassNumber() {
+		return classNumber;
+	}
+	public void setClassNumber(String classNumber) {
+		this.classNumber = classNumber;
+	}
+	
+	
+	
+}
