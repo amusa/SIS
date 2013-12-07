98f9dbee90be0351333717b64eb96593714e4f30
amusa <mayemij@gmail.com>
2013-12-07 09:03:26 -0500

    Revert "this must be reversed"
    This reverts commit e20ad83c42a3c4c56add3419398ccdc1ef5df4a2.

---
+++ build/generated-sources/ap-source-output/edu/rit/notification/entity/EnrollmentPK_.java
@@ -0,0 +1,14 @@
+package edu.rit.notification.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-10-26T23:44:40")
+@StaticMetamodel(EnrollmentPK.class)
+public class EnrollmentPK_ { 
+
+    public static volatile SingularAttribute<EnrollmentPK, String> uid;
+    public static volatile SingularAttribute<EnrollmentPK, String> classNumber;
+
+}
